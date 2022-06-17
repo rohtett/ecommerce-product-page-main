78a4ef35b44fd86fea56ff2ac8cb61e3542e1488
Rohit <raheet@hotmail.co.uk>
2022-06-17 13:41:19 +0100

    hamburger menu functionality and modal

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,29 @@
 "use strict";
-const quantity = document.getElementById("quantity");
-let value = quantity.value;
-const decrement = document.getElementById("decrement");
-const increment = document.getElementById("increment");
-const addCart = document.getElementById("add-cart");
+//hamburger functionality
+const modal = document.getElementById("modalBg");
+const showModal = display => {
+  modal.style.display="block";
+  eval("show" + display + "()");
+  modal.addEventListener("click", () => hideModal(display));
+}
+const hideModal = display => {
+  modal.style.display = "none";
+  eval("hide" + display + "()");
+}
+const hamburger = document.querySelector("#hamburger img")
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const showHamburger = () => {
+  hamburgerMenu.style.display="block";
+  hamburger.src="images/icon-close.svg";
+  hamburger.addEventListener("click", () => hideModal("Hamburger"))
+}
+const hideHamburger = () => {
+  hamburgerMenu.style.display="none";
+  hamburger.src="images/icon-menu.svg";
+  hamburger.addEventListener("click", () => showModal("Hamburger"));
+}
+
+//image display desktop
 const thumbnails = document.querySelectorAll("#display-thumbnail img");
 const displayImg = document.getElementById("display-large");
 let selected
@@ -18,17 +38,31 @@ const Select = (nodeIndex) => {
   source = source + ".jpg";
   displayImg.src=source
 }
+
+let value = quantity.value;
 function CartItem(name, value) {
   this.name = name;
   this.quantity = value;
 }
 
 window.onload = () => {
+  //quantity input changes
+  const quantity = document.getElementById("quantity");
+  quantity.addEventListener("change", () => {
+    value = quantity.value;
+    if (value < 1) {
+      quantity.value = 1;
+    }
+  })
+  //increment button functionality
+  const increment = document.getElementById("increment");
   increment.addEventListener("click", () => {
     value = quantity.value;
     value++;
     quantity.value = value;
   })
+  //decrement button functionality
+  const decrement = document.getElementById("decrement");
   decrement.addEventListener("click", () => {
     value = quantity.value;
     if (value >= 2) {
@@ -36,21 +70,20 @@ window.onload = () => {
       quantity.value = value;
     }
   })
-  quantity.addEventListener("change", () => {
-    value = quantity.value;
-    if (value < 1) {
-      quantity.value = 1;
-    }
-  })
+  //add to cart button functionality
+  const addCart = document.getElementById("add-cart");
   addCart.addEventListener("click", () => {
     value = quantity.value;
     let LEFallSneakers = new CartItem("Fall Limited Edition Sneakers", value);
     sessionStorage.setItem(LEFallSneakers.name, LEFallSneakers.quantity);
   })
+  //"carousel" functionality
   thumbnails.forEach((node,index) => {
     node.addEventListener("click", () => {
       Select(index);
     })
   })
+  //hamburger menu invokation
+  hamburger.addEventListener("click", () => showModal("Hamburger"));
   Select(0)
 }
