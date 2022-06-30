3e0bc7a69ba9743cdf14bb49957888d77f4d86b3
Rohit <raheet@hotmail.co.uk>
2022-06-30 06:10:36 +0100

    cart display and update functionality implemented

--- static/js/index.js
+++ static/js/index.js
@@ -24,20 +24,6 @@ const hideHamburger = () => {
   hamburger.addEventListener("click", () => showModal("Hamburger"));
 }
 
-//cart option stuff
-let value = quantity.value;
-function CartItem(name, value, price) {
-  this.name = name;
-  this.quantity = value;
-  this.price = price.slice(2);
-  this.subtotal = () => value * price;
-}
-const updateCart = () => {
-  for (let i in sessionStorage) {
-    console.log(JSON.parse(sessionStorage[i][1]));
-  }
-}
-
 //image display desktop
 const thumbnails = document.querySelectorAll("#display-thumbnail img");
 const displayImg = document.querySelector("#display-large img");
@@ -55,6 +41,74 @@ const carouselNext = bool => {
     nodeIndex = nodeIndex - 2;
   }
   select(nodeIndex);
+}
+
+//cart option stuff
+let value = quantity.value;
+function CartItem(name, picture, quantity, price) {
+  this.name = name;
+  this.picture = picture
+  this.quantity = quantity;
+  this.price = price.substring(1,price.length);
+}
+
+//cart display functions
+const cart = document.getElementById("cart-holder")
+const cartOn = () => {
+  updateCart();
+  setTimeout(() => {
+    cart.style.display = "flex";
+  },10);
+  document.addEventListener("click", () => {
+    let ev = event.target;
+    ev = ($(ev).parents())
+    let cartFound = false;
+    for (let i=0; i < ev.length; i++) {
+      if (ev[i] === cart) {
+        cartFound = true;
+      }
+    }
+    if (!cartFound) {
+      cartOff();
+    }
+  })
+}
+const cartOff = () => {
+  if (cart.style.display == "flex") {
+    cart.style.display = "none";
+  };
+}
+
+const updateCart = () => {
+  const cart = document.getElementById("cart-content");
+  const cartItems = document.querySelectorAll(".cart-content")
+  if (cartItems) {
+    cartItems.forEach(item => {
+      item.parentElement.removeChild(item)
+    })
+  }
+  if (sessionStorage.length > 0) {
+    const template = document.getElementById("cart-item");
+    for (let i = 0; i < sessionStorage.length; i++) {
+      let cartItem = JSON.parse(sessionStorage.getItem(sessionStorage.key(i)))
+      const item = template.content.cloneNode(true);
+      item.querySelector("img").src = cartItem.picture;
+      item.querySelector(".itemName").textContent = cartItem.name;
+      item.querySelector(".itemPrice").textContent = "$" + cartItem.price + " x " + cartItem.quantity;
+      item.querySelector(".itemSub").innerHTML = "$" + (cartItem.price * cartItem.quantity).toFixed(2);
+      cart.appendChild(item);
+    }
+  } else {
+    const placeholder = document.getElementById("empty-cart");
+    const text = placeholder.content.cloneNode(true);
+    cart.appendChild(text);
+  }
+}
+const deleteItem = () => {
+  let ev = event.target.parentElement;
+  //for now, i guess??
+  sessionStorage.clear()
+  ev.parentElement.removeChild(ev);
 }
 window.onload = () => {
   //hamburger menu invocation
@@ -90,13 +144,18 @@ window.onload = () => {
   //add to cart button functionality
   const addCart = document.getElementById("add-cart");
   addCart.addEventListener("click", () => {
+    let picture = document.querySelector("#display-thumbnail img").src;
     let price = document.getElementById("final-price").innerHTML;
     value = [quantity.value];
-    let LEFallSneakers = new CartItem("Fall Limited Edition Sneakers", value, price);
-    sessionStorage.setItem("Cart", JSON.stringify(LEFallSneakers));
-    //updateCart();
+    let LEFallSneakers = new CartItem("Fall Limited Edition Sneakers", picture, value, price);
+    sessionStorage.setItem(LEFallSneakers.name, JSON.stringify(LEFallSneakers));
   })
-  //updateCart();
+
+  //cart show functionality
+  const cartMenu = document.getElementById("cart-menu")
+  cartMenu.addEventListener("click", () => {
+    cartOn();
+  })
 
   //"carousel" functionality
   thumbnails.forEach((node,index) => {
